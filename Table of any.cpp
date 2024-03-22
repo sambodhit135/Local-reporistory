2a860d2f84ff95ae4f829f45423c967b128a74e4
sambodhit-chavhan <162886049+sambodhit-chavhan@users.noreply.github.com>
2024-03-22 21:04:11 +0530

    Add intial file

---
+++ Table of any.cpp
@@ -0,0 +1,13 @@
+#include<ioastream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter any positive number : ";
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<n<<" x "<<i<<"i*n"<<endl;
+    }
+    retrun 0;
+}
