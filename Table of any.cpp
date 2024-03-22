97645ec99cc4d437b81b1ef2732fbac2e91455d0
sambodhit-chavhan <162886049+sambodhit-chavhan@users.noreply.github.com>
2024-03-22 21:29:28 +0530

    Add the comments

--- Table of any.cpp
+++ Table of any.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter any positive number : ";
+    cout<<"Enter any positive number : "; //Taking input from the user
     cin>>n;
     for(int i=1;i<=10;i++)
     {
-        cout<<n<<" x "<<i<<"i*n"<<endl;
+        cout<<n<<" x "<<i<<"i*n"<<endl; //calculating and printing
     }
     retrun 0;
 }
